2b5420e5308f3d8fa3db77706cfa6cbab76a9c4c
mk-bluebird <2bf5zk7ksg@privaterelay.appleid.com>
2026-05-16 15:29:19 -0700

    Update panic_watchdog.rs

--- src/signer/panic_watchdog.rs
+++ src/signer/panic_watchdog.rs
@@ -66,19 +66,3 @@ fn send_force_close<C: BostromClient>(
     client.broadcast_tx(msg)?;
     Ok(())
 }
-
-// filename: src/bci/metrics.rs
-// destination: eco_restoration_shard/src/bci/metrics.rs
-// repo-target: github.com/mk-bluebird/eco_restoration_shard
-
-#[derive(Clone, Debug)]
-pub struct NeuroMetrics {
-    pub fearrate: f32,
-    pub coherence: f32,
-}
-
-pub trait BciInterface {
-    fn verify_connection(&self) -> super::health::BciHealthStatus;
-    fn read_neuro_metrics(&self) -> NeuroMetrics;
-    fn clone_interface(&self) -> Box<dyn BciInterface + Send>;
-}
