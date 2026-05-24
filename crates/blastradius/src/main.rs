210191563ddc3750efba2e27c0946eab506884c9
mk-bluebird <2bf5zk7ksg@privaterelay.appleid.com>
2026-05-23 19:02:50 -0700

    Create main.rs

---
+++ crates/blastradius/src/main.rs
@@ -0,0 +1,7 @@
+// filename: crates/blastradius/src/main.rs
+
+use blastradius::blastradius_kernel_version;
+
+fn main() {
+    println!("Blastradius kernel version: {}", blastradius_kernel_version());
+}
