ff7af0cad1ac94e670b22b2ee0bddb5ba14b29cd
mk-bluebird <2bf5zk7ksg@privaterelay.appleid.com>
2026-07-10 23:22:52 -0700

    Create lib.rs

---
+++ crates/beecorridor-ledger/src/lib.rs
@@ -0,0 +1,11 @@
+// filename: crates/beecorridor-ledger/src/lib.rs
+// destination: github.com/mk-bluebird/Prometheus-Praxis
+
+#![forbid(unsafe_code)]
+
+pub mod schema;
+pub mod types;
+pub mod query;
+pub mod telemetry;
+pub mod governance;
+pub mod kani_harnesses;
