101e714b1d3044022a79d5e0ba198cfa4b834741
mk-bluebird <2bf5zk7ksg@privaterelay.appleid.com>
2026-08-02 01:40:52 -0700

    Create urban_climate_model_evaluation.rs

---
+++ src/urban_climate/evaluation/src/urban_climate_model_evaluation.rs
@@ -0,0 +1,6 @@
+use alncore::parse_aln_str; // as referenced in governance spine.[8]
+use serde::{Deserialize, Serialize};
+
+// ... ComponentKind, UrbanClimateModelComponent, UrbanClimateModelEvaluationEnvelope,
+// thresholds, EligibilityVerdict, EligibilityDecision, min_scores_invariant,
+// enforce_urban_climate_model_eligibility ...
