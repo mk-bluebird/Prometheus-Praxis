f9debf8db8339d22e2a0eb092cf9aa2d5f498dbf
mk-bluebird <2bf5zk7ksg@privaterelay.appleid.com>
2026-08-10 02:59:54 -0700

    Create WorkloadFrame.kt

---
+++ kotlin/cyboquatic/src/main/kotlin/org/prometheuspraxis/cyboquatic/generated/WorkloadFrame.kt
@@ -0,0 +1,15 @@
+// File: kotlin/cyboquatic/src/main/kotlin/org/prometheuspraxis/cyboquatic/generated/WorkloadFrame.kt
+// Generated from aln/cyboquatic/workload_corridor_2026_08_09.aln; do not edit manually.
+package org.prometheuspraxis.cyboquatic.generated
+
+data class WorkloadFrame(
+    val owner_did: String,
+    val energyreqJ: Double,
+    val deltaVt: Double,
+    val K: Double,
+    val E: Double,
+    val R: Double,
+    val fog_confidence: Double,
+    val eco_impact_value: Double,
+    val canal_node: String
+)
