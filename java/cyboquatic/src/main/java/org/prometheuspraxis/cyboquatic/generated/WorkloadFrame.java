8b008422815cdd3ee29e9f093774f81bf51df454
mk-bluebird <2bf5zk7ksg@privaterelay.appleid.com>
2026-08-10 02:55:45 -0700

    Create WorkloadFrame.java

---
+++ java/cyboquatic/src/main/java/org/prometheuspraxis/cyboquatic/generated/WorkloadFrame.java
@@ -0,0 +1,15 @@
+// File: java/cyboquatic/src/main/java/org/prometheuspraxis/cyboquatic/generated/WorkloadFrame.java
+// Generated from aln/cyboquatic/workload_corridor_2026_08_09.aln; do not edit manually.
+package org.prometheuspraxis.cyboquatic.generated;
+
+public record WorkloadFrame(
+    String owner_did,
+    double energyreqJ,
+    double deltaVt,
+    double K,
+    double E,
+    double R,
+    double fog_confidence,
+    double eco_impact_value,
+    String canal_node
+) {}
