e6eee5f77f2f8a90f27f45fc825218bbf18f1a17
mk-bluebird <2bf5zk7ksg@privaterelay.appleid.com>
2026-08-12 03:17:24 -0700

    Create commitlint.config.cjs

---
+++ commitlint.config.cjs
@@ -0,0 +1,11 @@
+// File: commitlint.config.cjs
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 100],
+    "type-enum": [2, "always", [
+      "build", "chore", "ci", "docs", "feat", "fix",
+      "perf", "refactor", "revert", "style", "test"
+    ]]
+  }
+};
