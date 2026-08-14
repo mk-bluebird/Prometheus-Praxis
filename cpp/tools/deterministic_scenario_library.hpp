025af578406109ae9bd12a9b33614535c8d06ad0
mk-bluebird <2bf5zk7ksg@privaterelay.appleid.com>
2026-08-14 02:19:45 -0700

    Update deterministic_scenario_library.hpp

--- cpp/tools/deterministic_scenario_library.hpp
+++ cpp/tools/deterministic_scenario_library.hpp
@@ -7,14 +7,14 @@
 
 #include "../simulation/irrigation_scenario_diagnostics.hpp"
 
+namespace prometheus_praxis::foundation::scenario {
+
 class DeterministicScenarioGenerator {
 public:
     explicit DeterministicScenarioGenerator(std::uint64_t seed) noexcept;
 
     std::uint64_t NextUnsigned() noexcept;
-
     double NextUnitInterval() noexcept;
-
     double NextInRange(double minimum, double maximum) noexcept;
 
     std::vector<double> GenerateUnitIntervalValues(std::size_t count);
@@ -24,8 +24,20 @@ public:
         std::size_t horizon,
         double maximum_rain);
 
+    std::uint64_t State() const noexcept;
+    void Reset(std::uint64_t seed) noexcept;
+
 private:
-    std::uint64_t state_;
+    std::uint64_t state_{};
 };
 
+std::vector<double> GenerateProbabilities(std::size_t count);
+
+std::vector<double> GenerateBoundedValues(
+    std::size_t count,
+    double minimum,
+    double maximum);
+
 bool DeterministicScenarioLibrarySelfTest();
+
+}  // namespace prometheus_praxis::foundation::scenario
